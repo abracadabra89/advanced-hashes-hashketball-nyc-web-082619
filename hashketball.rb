920df8223c22ee4a4deeb98ddbfae34341ba7427
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-16 13:33:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -101,16 +101,7 @@ def game_hash
          :steals => 1,
          :blocks => 1,
          :slam_dunks => 0   
-        },
-        "Brendan Haywood" => {
-         :number => 33,
-         :shoe => 15,
-         :points => 6,
-         :rebounds => 12,
-         :assists => 12,
-         :steals => 22,
-         :blocks => 5,
-         :slam_dunks => 12   
+        }
         }
       }
     }
